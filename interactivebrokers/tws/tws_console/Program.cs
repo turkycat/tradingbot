e77088185fce6a08c6a320a22b0136a26ad7c4aa
Jesse Frush <jesse.frush@gmail.com>
2020-03-21 21:51:54 -0700

    added a basic tws project with a client interface, no implementations.

---
+++ interactivebrokers/tws/tws_console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tws_lib;
+
+namespace tws_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing...");
+            TwsClient client = new TwsClient();
+        }
+    }
+
+    
+}
